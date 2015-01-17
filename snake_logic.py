0d702bbc848ec66ad46faf4d3a5c11da29dfc0c3
zerovirus123 <briansia93@gmail.com>
2015-01-16 23:54:11 -0800

    lets call it a day for now

--- snake_logic.py
+++ snake_logic.py
@@ -3,7 +3,7 @@ from random import *
 #for easier reading
 row = 0
 col = 1
-empty = 0
+EMPTY = 0
 snake_body = 1
 apple = 2
 
@@ -11,46 +11,43 @@ board_size = 20
 apple = []
 
 class snake_board():
-	""" for board, 0 for empty, 1 for snake, 2 for apple
-			"""
-	def __init__():
-		board = []
-		for _ in range(board_size):
-		    board.append(range(board_size))
+    """ for board, 0 for EMPTY, 1 for snake, 2 for apple
+            """
+    def __init__():
+        self.board = []
+        for _ in range(board_size):
+            self.board.append(range(board_size))
 
-		for row in board:
-		    for col in board[row]:
-			board[row][col].append(empty)
+        for row in board:
+            for col in board[row]:
+            self.board[row][col].append(EMPTY)
 
-	def generate_apple():
-	   empty_space = False
-	   apple_x = random(board_size)
-	   apple_y = random(board_size)
-	   while empty_space = False:
-	       if board[apple_x, apple_y] == snake_body \
-		or board[apple_x, apple_y] == apple:
-	          continue
-	       else:
-		  board[apple_x,apple_y] = apple
-			    	
+    def generate_apple():
+       EMPTY_space = False
+       apple_x = random(board_size)
+       apple_y = random(board_size)
+       while EMPTY_space = False:
+           if self.board[apple_x, apple_y] == snake_body \
+        or self.board[apple_x, apple_y] == apple:
+              continue
+           else:
+          self.board[apple_x,apple_y] = apple
+                    
 
 class snake():
-	def __init__():
-	    dead = False
-	    score = 0
-	    body_length = 3	
-	    head = [9,9]
-	    body = [[10,10], [11,11], [12,12]]
+    def __init__():
+        self.dead = False
+        self.score = 0
+        self.body_length = 3    
+        self.head = [9,9]
+        self.body = [[10,10], [11,11], [12,12]]
 
+    def collide():
+        if self.head in self.body:
+            self.dead = True
+        if self.head[row]<0 or self.head[row]>=board_size or self.head[col]<0 \          or self.head[col] >= board_size: 
+            self.dead = True
+        if 	 
 
-	def collide():
-	    if head in body:
-		snake.dead = True
-	    if head[row]<0 or head[row]>=board_size or head[col]<0 \
-	        or head[col] >= board_size: 
-		snake.dead = True
-
-	def move():
-	    pass
-
-
+    def move():
+        pass
